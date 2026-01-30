00320d116f109c37e4c7961a4dcdbf1db7bd7355
okla <87949054+okthela@users.noreply.github.com>
2026-01-30 17:42:23 -0300

    added ram usage and colors

--- genfetch.py
+++ genfetch.py
@@ -6,6 +6,7 @@ import os
 import pyfiglet
 import cpuinfo
 import platform
+from decimal import Decimal, ROUND_DOWN
 import psutil
 from pathlib import Path
 
@@ -21,28 +22,16 @@ cpu_percentage = psutil.cpu_percent()
 cores = os.popen("nproc").read().strip()
 ram_max1 = psutil.virtual_memory()
 ram_max = ram_max1.total / (1024 ** 3)
-ram_util = ram_max1.available / (1024 ** 3)
+ram_avaliable = ram_max1.available / (1024 ** 3)
 uptime = time.time() - psutil.boot_time()
 disks = psutil.disk_partitions()
 packages = os.popen("find /var/db/pkg -mindepth 2 -maxdepth 2 -type d | wc -l").read().strip()
 shell = os.environ.get("SHELL")
 theme = os.popen("gsettings get org.gnome.desktop.interface gtk-theme").read().strip()
 term = os.environ.get("TERM")
-
-if not hostname_check.exists():
-    print("Hostname isnt set! (failed to check for /etc/hostname)")
-    exit()
-
-gpu1 = os.popen("lspci | grep VGA").read().strip()
-
-gpu = gpu1.replace("10:00.0 VGA compatible controller:", "")
-
-if not cpu_thang.exists():
-    print("CPU Info doesnt exist, creating it (this avoids long startups)")
-    cpu_not_real1 = cpuinfo.get_cpu_info()
-    cpu_not_real = cpu_not_real1["brand_raw"]
-    os.system(f"touch ~/.config/cpu.info && echo '{cpu_not_real}' >> ~/.config/genfetch_cpu.info")
-
+line = "- "
+square = "■"
+ram_usage = int(ram_max) - int(ram_avaliable)
 black   = os.popen("tput setaf 0").read()
 red     = os.popen("tput setaf 1").read()
 green   = os.popen("tput setaf 2").read()
@@ -52,22 +41,32 @@ magenta = os.popen("tput setaf 5").read()
 cyan    = os.popen("tput setaf 6").read()
 white   = os.popen("tput setaf 7").read()
 reset   = os.popen("tput sgr0").read()
+
+if not hostname_check.exists():
+    print("Hostname isnt set! (failed to check for /etc/hostname)")
+    exit()
+
+if not cpu_thang.exists():
+    print("CPU Info doesnt exist, creating it (this avoids long startups)")
+    cpu_not_real1 = cpuinfo.get_cpu_info()
+    cpu_not_real = cpu_not_real1["brand_raw"]
+    os.system(f"touch ~/.config/cpu.info && echo '{cpu_not_real}' >> ~/.config/genfetch_cpu.info")
+
 logo1 = pyfiglet.figlet_format(f"Gentoo Linux")
-print(f"{red}|------------------------------------------------------------------------|")
 print(f"{red}{logo1}")
-print("|------------------------------------------------------------------------|")
-print(f"{red}|{red}{'PC 󰍹':<{label_width}} {whoami}@{whoispc}")
-print(f"{red}|{red}{'CPU ':<{label_width}} {cpu} x {cores}, util: {cpu_percentage}%")
-print(f"{cyan}|{cyan}{'GPU 󰢮':<{label_width}}{gpu}")
-print(f"{cyan}|{cyan}{'Shell ':<{label_width}} {shell}")
-print(f"{magenta}|{magenta}{'Terminal  ':<{label_width}} {term}")
-print(f"{magenta}|{magenta}{'Kernel ':<{label_width}} {platform.release()}")
-print(f"{magenta}|{magenta}{'Packages 󰉍 ':<{label_width}} {packages} installed")
-print(f"{green}|{green}{'Theme ':<{label_width}} {theme}")
-print(f"{green}|{green}{'RAM ':<{label_width}} {ram_max:.1f} GiB ({ram_util:.1f} GiB available)")
-print(f"{green}|{green}{'Storage 󱊟':<{label_width}}")
+print(f"{red}{line}{red}{'PC 󰍹':<{label_width}} {whoami}@{whoispc}")
+print(f"{red}{line}{red}{'CPU ':<{label_width}} {cpu} x {cores}, util: {cpu_percentage}%")
+print(f"{red}{line}{red}{'GPU 󰢮':<{label_width}}{gpu}")
+print(f"{red}{line}{red}{'Kernel ':<{label_width}} {platform.release()}")
+print(f"{red}{line}{red}{'RAM ':<{label_width}} {ram_max:.1f} GiB ({ram_usage} GiB used)")
+print(f"{red}{line}{red}Uptime   {uptime / 3600:.1f} hours")
+print(f"{red}{line}{red}{'Storage 󱊟 :':<{label_width}}")
 for parts in disks:
     util = psutil.disk_usage(parts.mountpoint)
-    print(f"{blue} * Device  {parts.device} Mountpoint: '{parts.mountpoint}' usage: {util.used / (1024**3):.2f} GiB out of {util.total / (1024**3):.2f} GiB")
-print(f"{magenta}|{magenta}Uptime   {uptime / 3600:.1f} hours")
-print(f"{magenta}|------------------------------------------------------------------------|")
+    print(f"{blue}  * Device  {parts.device} Mountpoint: '{parts.mountpoint}' usage: {util.used / (1024**3):.2f} GiB out of {util.total / (1024**3):.2f} GiB")
+print(f"{magenta}{line}{magenta}{'Terminal  ':<{label_width}} {term}")
+print(f"{magenta}{line}{magenta}{'Shell ':<{label_width}} {shell}")
+print(f"{magenta}{line}{magenta}{'Packages 󰉍 ':<{label_width}} {packages} (portage)")
+print(f"{magenta}{line}{magenta}{'Theme ':<{label_width}} {theme}")
+print(f"{magenta}{line}{square} {red}{square} {green}{square} {yellow}{square}{reset}")
+print(f"{blue}{line}{square} {magenta}{square} {cyan}{square} {white}{square}{reset}")
